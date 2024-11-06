74099bdcc97656f17b2e63760222577c8940bc49
Mark <mlacay@berkeley.edu>
2024-11-06 10:39:32 -0800

    Fix how server/client get instantiated

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,6 @@
 """FedRecPolicyEval: A Flower / PyTorch package for evaluating recommender system policies."""
+
+from src.server.server import create_server_app
+from src.client.client import create_client_app
+
+__all__ = ["create_server_app", "create_client_app"]
